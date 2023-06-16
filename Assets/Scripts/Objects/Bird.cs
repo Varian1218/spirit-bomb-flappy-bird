1222aa9caba4d1e5555796ad1825f6553231e4f0
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-06-16 12:35:40 +0700

    add bird

---
+++ Assets/Scripts/Objects/Bird.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Objects
+{
+    public class Bird : MonoBehaviour
+    {
+        [SerializeField] private float gravityScale = .6f;
+        [SerializeField] private float radius;
+
+        public Vector3 Position
+        {
+            get => transform.position;
+            set => transform.position = value;
+        }
+
+        public float PositionY
+        {
+            get => transform.position.y;
+            set
+            {
+                var position = transform.position;
+                position.y = value;
+                transform.position = position;
+            }
+        }
+
+        public float Radius => radius;
+
+        public Vector3 Velocity { private get; set; }
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, radius);
+        }
+#endif
+        public void Step(float dt)
+        {
+            Velocity += gravityScale * dt * Physics.gravity;
+            transform.position += Velocity * dt;
+        }
+    }
+}
