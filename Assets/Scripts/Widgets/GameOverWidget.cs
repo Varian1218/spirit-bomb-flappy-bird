fbbfc116917ae26434ba3fe22991938ce0c3bfd4
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-06-16 12:36:56 +0700

    add game over

---
+++ Assets/Scripts/Widgets/GameOverWidget.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Widgets
+{
+    public class GameOverWidget : MonoBehaviour
+    {
+        public bool Visible
+        {
+            set => gameObject.SetActive(value);
+        }
+
+        public void OnButtonClicked()
+        {
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+}
