572e70b505d576de49ad6105d198e1c2efebd923
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-06-16 12:32:23 +0700

    add physics utils

---
+++ Assets/Scripts/Utils/PhysicsUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utils
+{
+    public static class PhysicsUtils
+    {
+        public static bool Overlap(Vector2 circlePosition, float circleRadius, Vector2 rectPos,
+            Vector2 rectSize)
+        {
+            return Overlap(circlePosition.x, circlePosition.y, circleRadius, rectPos.x - rectSize.x / 2,
+                rectPos.y - rectSize.y / 2, rectSize.y, rectSize.x);
+        }
+
+        public static bool Overlap(float circleX, float circleY, float circleRadius, float rectX, float rectY,
+            float rectHeight, float rectWidth)
+        {
+            var closestX = Math.Clamp(circleX, rectX, rectX + rectWidth);
+            var closestY = Math.Clamp(circleY, rectY, rectY + rectHeight);
+            var distanceX = circleX - closestX;
+            var distanceY = circleY - closestY;
+            var distanceSquared = distanceX * distanceX + distanceY * distanceY;
+            return distanceSquared <= circleRadius * circleRadius;
+        }
+    }
+}
